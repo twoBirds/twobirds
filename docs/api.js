87a138e7450223646046e3869915ddb0db53783a
Frank Thürigen <frank_thuerigen@yahoo.de>
2015-10-14 08:27:13 +0000

    added preliminary docs, work in progress

---
+++ docs/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TbEvent",
+        "TbSelector",
+        "tb",
+        "tb.Model"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
